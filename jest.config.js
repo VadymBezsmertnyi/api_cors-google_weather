67fe8a08e23b6c8c22a234acc618167217bca14b
vados <vados199368@gmail.com>
2022-08-08 16:24:41 +0300

    fix error export token

--- jest.config.js
+++ jest.config.js
@@ -32,13 +32,18 @@ module.exports = async () => {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     moduleNameMapper: {
       components: ['<rootDir>/src/components'],
-      'constants/cities': ['<rootDir>/src/constants/cities'],
+      'constants/cities': ['<rootDir>/src/constants/cities/'],
       hooks: ['<rootDir>/src/hooks'],
       interface: ['<rootDir>/src/interface'],
       pages: ['<rootDir>/src/pages'],
       reducers: ['<rootDir>/src/reducers'],
       store: ['<rootDir>/src/store'],
       theme: ['<rootDir>/src/theme'],
+
+      '^d3-(.*)$': `d3-$1/dist/d3-$1`,
     },
+    transformIgnorePatterns: [
+      '/node_modules/(?!d3|d3-array|internmap|delaunator|robust-predicates)',
+    ],
   };
 };
