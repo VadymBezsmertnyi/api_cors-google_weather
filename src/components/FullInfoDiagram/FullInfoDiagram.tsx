ccf93919131e91d1c0ea5ff3e1655302846a28f3
vados <vados199368@gmail.com>
2022-08-09 11:41:38 +0300

    Completed 100%

--- src/components/FullInfoDiagram/FullInfoDiagram.tsx
+++ src/components/FullInfoDiagram/FullInfoDiagram.tsx
@@ -1,5 +1,15 @@
 import { DiagramDailyType } from 'interface';
 import { useEffect, useState } from 'react';
+import {
+  XYPlot,
+  VerticalGridLines,
+  VerticalBarSeries,
+  BarSeries,
+  LabelSeries,
+  HorizontalGridLines,
+  XAxis,
+  YAxis,
+} from 'react-vis';
 
 interface IFullInfoDiagramProps {
   diagram: DiagramDailyType[];
@@ -18,7 +28,32 @@ const FullInfoDiagram = ({
     setWidthNow(window.innerWidth - 50);
   }, [window]);
 
-  return <>diagram</>;
+  const data = diagram?.map(({ day, temp }) => {
+    return { x: day, y: temp };
+  });
+
+  return (
+    data?.length && (
+      <XYPlot
+        animation
+        xType="ordinal"
+        width={widthNow}
+        height={800}
+        xDistance={100}
+      >
+        <VerticalGridLines />
+        <HorizontalGridLines />
+        <XAxis />
+        <YAxis />
+        <VerticalBarSeries
+          sx={{ background: 'red' }}
+          className="vertical-bar-series-example"
+          data={data}
+        />
+        <LabelSeries data={data} getLabel={(d) => `${d.y}°C`} />
+      </XYPlot>
+    )
+  );
 };
 
 export default FullInfoDiagram;
